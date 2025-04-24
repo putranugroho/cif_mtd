0a7ff5b88c85cef0f4305e49a8f432862c962ec4
Edi Kurniawan <edikurniawan@Edis-MacBook-Pro.local>
2025-04-24 23:57:40 +0700

    update

--- lib/module/setup/aktivasi/aktivasi_notifier.dart
+++ lib/module/setup/aktivasi/aktivasi_notifier.dart
@@ -1,14 +1,42 @@
+import 'dart:convert';
+
 import 'package:accounting/models/index.dart';
+import 'package:accounting/repository/SetupRepository.dart';
+import 'package:accounting/utils/button_custom.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../../network/network.dart';
+
+import 'package:time_picker_spinner/time_picker_spinner.dart';
 
 class AktivasiNotifier extends ChangeNotifier {
   final BuildContext context;
 
   AktivasiNotifier({required this.context}) {
-    for (Map<String, dynamic> i in json) {
-      list.add(AktivasiModel.fromJson(i));
-    }
+    getAktivasi();
     notifyListeners();
+  }
+
+  var isLoading = true;
+  Future getAktivasi() async {
+    isLoading = true;
+    list.clear();
+    notifyListeners();
+    var data = {"kode_pt": "001"};
+    Setuprepository.setup(token, NetworkURL.getAktivasi(), jsonEncode(data))
+        .then((value) {
+      if (value['status'].toString().toLowerCase().contains("success")) {
+        for (Map<String, dynamic> i in value['data']) {
+          list.add(AktivasiModel.fromJson(i));
+        }
+        isLoading = false;
+        notifyListeners();
+      } else {
+        isLoading = false;
+        notifyListeners();
+      }
+    });
   }
 
   bool dialog = false;
@@ -26,6 +54,170 @@ class AktivasiNotifier extends ChangeNotifier {
   TextEditingController nama = TextEditingController();
   TextEditingController jamMulai = TextEditingController();
   TextEditingController jamSelesai = TextEditingController();
+  DateTime jamMulaiTime = DateTime.now();
+  DateTime jamSelesaiTime = DateTime.now();
+  pilihJamMulai() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return StatefulBuilder(builder: (context, setState) {
+            return Dialog(
+              child: Container(
+                width: 600,
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            "Pilih Jam Mulai",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                            width: 30,
+                            height: 30,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.grey[300]),
+                            child: Icon(
+                              Icons.close,
+                              size: 12,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    TimePickerSpinner(
+                      locale: const Locale('en', ''),
+                      time: jamMulaiTime,
+                      is24HourMode: true,
+                      isShowSeconds: false,
+                      itemHeight: 80,
+                      normalTextStyle: const TextStyle(
+                        fontSize: 24,
+                      ),
+                      highlightedTextStyle:
+                          const TextStyle(fontSize: 24, color: Colors.blue),
+                      isForce2Digits: true,
+                      onTimeChange: (time) {
+                        setState(() {
+                          jamMulaiTime = time;
+
+                          notifyListeners();
+                        });
+                      },
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    ButtonPrimary(
+                      onTap: () {
+                        Navigator.pop(context);
+                        jamMulai.text =
+                            DateFormat('HH:mm').format(jamMulaiTime);
+                      },
+                      name: "Simpan",
+                    )
+                  ],
+                ),
+              ),
+            );
+          });
+        });
+  }
+
+  pilihJamSelesai() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return StatefulBuilder(builder: (context, setState) {
+            return Dialog(
+              child: Container(
+                width: 600,
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            "Pilih Jam Selesai",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                            width: 30,
+                            height: 30,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.grey[300]),
+                            child: Icon(
+                              Icons.close,
+                              size: 12,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    TimePickerSpinner(
+                      locale: const Locale('en', ''),
+                      time: jamSelesaiTime,
+                      is24HourMode: true,
+                      isShowSeconds: false,
+                      itemHeight: 80,
+                      normalTextStyle: const TextStyle(
+                        fontSize: 24,
+                      ),
+                      highlightedTextStyle:
+                          const TextStyle(fontSize: 24, color: Colors.blue),
+                      isForce2Digits: true,
+                      onTimeChange: (time) {
+                        setState(() {
+                          jamSelesaiTime = time;
+
+                          notifyListeners();
+                        });
+                      },
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    ButtonPrimary(
+                      onTap: () {
+                        Navigator.pop(context);
+                        jamSelesai.text =
+                            DateFormat('HH:mm').format(jamSelesaiTime);
+                      },
+                      name: "Simpan",
+                    )
+                  ],
+                ),
+              ),
+            );
+          });
+        });
+  }
+
   List<String> listHari = [
     "Senin",
     "Selasa",
@@ -52,20 +244,4 @@ class AktivasiNotifier extends ChangeNotifier {
   }
 
   List<AktivasiModel> list = [];
-  List<Map<String, dynamic>> json = [
-    {
-      "kd_aktivasi": "100001",
-      "nm_aktivasi": "Regu 1",
-      "hari": "['Senin','Selasa','Rabu']",
-      "jam_mulai": "10:00:00",
-      "jam_selesai": "17:00:00"
-    },
-    {
-      "kd_aktivasi": "100002",
-      "nm_aktivasi": "Regu 2",
-      "hari": "['Kamis','Jumat','Sabtu']",
-      "jam_mulai": "08:00:00",
-      "jam_selesai": "17:00:00"
-    },
-  ];
 }
